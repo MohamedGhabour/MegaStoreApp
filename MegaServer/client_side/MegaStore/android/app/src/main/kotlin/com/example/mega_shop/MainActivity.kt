1fe9aebfefc0e76ae30157ba0cdea445719239c3
MohamedGhabour <mohamed.m.ghabour@gmail.com>
2025-10-30 23:47:16 +0300

    Remove client_side folder from backend

--- MegaServer/client_side/MegaStore/android/app/src/main/kotlin/com/example/mega_shop/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mega_shop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- MegaServer/index.js
+++ MegaServer/index.js
@@ -73,7 +73,7 @@ app.use((error, req, res, next) => {
 });
 
 // Start server using PORT from Railway
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
